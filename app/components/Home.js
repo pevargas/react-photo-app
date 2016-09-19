284fcf25f7573f446a6d78f3ed698d0702c87ac8
Patrick Vargas <patrick@yesthisispatrick.com>
2016-09-18 20:37:48 -0400

    Display the result of the api call

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,9 +1,19 @@
 var React = require('react')
+var Loading = require('./Loading')
+
+function puke (object) {
+  return <pre>{JSON.stringify(object, null, ' ')}</pre>
+}
 
 function Home (props) {
+  if (props.isLoading === true) {
+    return (<Loading />)
+  }
+
   return (
     <div>
-      This is home
+      <p>This is home</p>
+      {puke(props)}
     </div>
   )
 }
--- app/containers/HomeContainer.js
+++ app/containers/HomeContainer.js
@@ -1,10 +1,28 @@
 var React = require('react')
 var Home = require('../components/Home')
+var photoHelper = require('../utils/photoHelper')
 
 var HomeContainer = React.createClass({
+  getInitialState: function () {
+    return {
+      isLoading: true,
+      data: {}
+    }
+  },
+  componentDidMount: function () {
+    photoHelper.getPopularPhotos()
+      .then(function (data) {
+        this.setState({
+          isLoading: false,
+          data: data
+        })
+      }.bind(this))
+  },
   render: function () {
     return (
-      <Home />
+      <Home
+        isLoading={this.state.isLoading}
+        data={this.state.data} />
     )
   }
 })
